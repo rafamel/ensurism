7e20b4695681873f5a9aa8f1051f199fa76c98ff
Rafa Mel <melrcm@gmx.net>
2020-02-28 23:49:45 +0100

    feat: adds assert function

---
+++ src/assert.ts
@@ -0,0 +1,30 @@
+import { Type, NotDefinedType } from './types';
+
+export type AssertValue<
+  T extends Type,
+  D extends boolean = false
+> = D extends true
+  ? Exclude<T, NotDefinedType> extends Array<infer U>
+    ? Array<Exclude<U, NotDefinedType>>
+    : Exclude<T, NotDefinedType> extends Record<string, any>
+    ? { [P in keyof T]: Exclude<T[P], NotDefinedType> }
+    : Exclude<T, NotDefinedType>
+  : Exclude<T, NotDefinedType>;
+
+export function assert<T extends Type, D extends boolean = false>(
+  data: T,
+  deep?: D
+): AssertValue<T, D> {
+  if (data === undefined) throw Error(`Data is not defined`);
+
+  if (deep && typeof data === 'object') {
+    const items = Array.isArray(data) ? data : Object.values(data as object);
+    for (const item of items) {
+      if (item === undefined) {
+        throw Error(`An inner value of data is not defined`);
+      }
+    }
+  }
+
+  return data as any;
+}
